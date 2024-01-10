99b9ffed7c74eac4fa93cc15f0cdac0d1c9f6f91
Fabrizio Angius <fabrizio.angius.4@gmail.com>
2024-01-10 19:18:50 +0100

    inizio US#2

---
+++ resources/views/announcement/index.blade.php
@@ -0,0 +1,13 @@
+{{-- RANVEER
+    - Annuncio: Titolo, categoria, prezzo, descrizione, foto segnaposto.
+    - Ordinazione: dal più recente, per categorie
+    - Extra: dal più vecchio, dal più economico, dal più caro
+--}}
+
+<x-main>
+
+    
+    
+   
+
+</x-main>
---
+++ resources/views/announcement/show.blade.php
@@ -0,0 +1,13 @@
+{{-- GABRI
+    - Dettaglio annuncio: CARD -> Titolo, categoria, prezzo, descrizione, foto segnaposto, venditore, data pubblicazione.
+    - Carosello: (lorem picsum) N-foto segnaposto
+--}}
+
+
+<x-main>
+
+    
+    
+   
+
+</x-main>
