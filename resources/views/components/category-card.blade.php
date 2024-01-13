840b85c2bdd871411a7896364719ed3561da563e
Fabrizio Angius <fabrizio.angius.4@gmail.com>
2024-01-13 22:41:52 +0100

    .

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -1,5 +1,5 @@
 <div class="card mb-3 mx-2 p-0" style="width: 50mm; height: 65mm">
-    <img src="https://picsum.photos/400" class="card-img-top" style="height: 40mm">
+    <img src="https://cdn-icons-png.flaticon.com/512/55/55283.png" class="card-img-top" style="height: 40mm">
     <div class="card-body py-2">
       <h5 class="card-text text-center"><a href="{{route('adsByCat',$category)}}">{{$title}}</a></h5>
       <p class="card-text text-center">
