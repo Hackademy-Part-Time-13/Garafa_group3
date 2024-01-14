b0d941723ed54dc9d4504beccc443bb4c45d0843
gabriele <cesaranogabriele02@gmail.com>
2024-01-14 20:30:45 +0100

    css

--- resources/views/user/auth/profile.blade.php
+++ resources/views/user/auth/profile.blade.php
@@ -1,58 +1,5 @@
 <x-main>
-
-<div class="container text-center">
-    <div >
-         <img src="https://picsum.photos/300" alt="" class="round-profile-imageprofile" >
-    </div>
-<h1>
-{{auth()->user()->name}}
-</h1>
     <div>
-       <button> 
-        <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btncheck1">Condividi</label>
-        </button>
-
-        <button>
-            <input type="checkbox" class="btn-check" id="btncheck2" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btncheck2">Modifica profilo</label>
-        </button>
-  
+       
     </div>
-
-    <div>
-        <button>
-            creati
-        </button>
-        <button>
-            salvati
-        </button>
-
-    </div>
-
-
-    <div>
-        <button>
-            crea annuncio
-        </button>
-    </div>
-
-
-        
-
-
-   
-   
-   
-
-
-
-
-
-
-
-
-
-</div>
-
 </x-main>
