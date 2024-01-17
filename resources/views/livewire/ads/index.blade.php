e4bc8a1d7b3364a415f9fb89e70f57a92d42cf73
Ranveer Shekhawat <Ranveershekhawat58@gmail.com>
2024-01-17 21:44:20 +0100

    css ads card

--- resources/views/livewire/ads/index.blade.php
+++ resources/views/livewire/ads/index.blade.php
@@ -4,28 +4,28 @@
 
         <div class="index_sort">
             <div class="index_sort_box mt-5">
-                <form class="header_form" action="{{route('ad.searched')}}">
+                <form class="header_form" action="{{ route('ad.searched') }}">
                     <input type="text" name="searched">
                     <button type="submit">Cerca</button>
                 </form>
             </div>
         </div>
 
-    {{-- BOTTONI SORT --}}
-    <div class="container mt-3 mb-3">
-        <div class="d-flex justify-content-end">
-            <div class="col-auto">
-                <select wire:model.change="$sortSelect" class="form-select">
-                    <option selected value="noSorted">Nessun ordine</option>
-                    <option value="newest">Dal più recente</option>
-                    <option value="oldest">Dal meno recente</option>
-                    <option value="cheapest">Dal più economico</option>
-                    <option value="mostExpensive">Dal più caro</option>
-                </select>
+        {{-- BOTTONI SORT --}}
+        <div class="container mt-3 mb-3">
+            <div class="d-flex justify-content-end">
+                <div class="col-auto">
+                    <select wire:model.change="$sortSelect" class="form-select">
+                        <option selected value="noSorted">Nessun ordine</option>
+                        <option value="newest">Dal più recente</option>
+                        <option value="oldest">Dal meno recente</option>
+                        <option value="cheapest">Dal più economico</option>
+                        <option value="mostExpensive">Dal più caro</option>
+                    </select>
+                </div>
             </div>
         </div>
-    </div>
-    
+
     </div>
 
 
@@ -34,34 +34,44 @@
         <div class="index_box">
             <div class="row g-5 px-3">
 
-            @foreach ($ads as $ad)
+                @foreach ($ads as $ad)
+                    <div class="col-3 d-flex justify-content-center">
+                        <div class="ads_container ">
 
-            <div class="col-4 d-flex justify-content-center">
-                <div class="ads_container ">
+                            <img class="ads_img_container" src="https://picsum.photos/400/400" alt="">
 
-                    <img class="ads_img_container" src="https://picsum.photos/400/400" alt="">
+                            <span class="ads_dettaglio_batton"><a href="{{ route('ad.show', $ad->id) }}">vista
+                                    dettaglio</a></span>
 
-                    <span class="ads_dettaglio_batton"><a href="{{ route('ad.show', $ad->id) }}">vista dettaglio</a></span>
-                    <p class="ads_titale">{{ $ad->title }}</p>
+                            <div class="ads_content"> 
+                                <div class="ads_text_box">
+                                    <p class="ads_titale ads_price">{{ $ad->price }} £</p>
+                                    <p class="ads_titale">{{ $ad->title }}</p>
+                                    <p class="ads_titale_description">{{ Str::limit($ad->description, 65) }}</p>
 
-                    <div class="ads_favorites">
+                                </div>
 
-                    @if (App\Models\Favorite::where('ad_id', $ad->id)->where('user_id', auth()->user()->id)->exists())
-                        <span wire:click="unlike({{$ad}})" class="d-flex"> <i class="bi bi-heart-fill"></i>
-                            <p>{{ $ad->favorites->count() }}</p>
-                        </span>
+                                <div class="ads_favorites">
 
-                    @else
-                        <span wire:click="liker({{$ad}})" class="d-flex"><i class="bi bi-heart"></i>
-                            <p>{{ $ad->favorites->count() }}</p>
-                        </span>
-                    @endif
+                                    @if (App\Models\Favorite::where('ad_id', $ad->id)->where('user_id', auth()->user()->id)->exists())
+                                        <span wire:click="unlike({{ $ad }})" class="d-flex ads_favorites_flex"> <i
+                                                class="bi bi-heart-fill"></i>
+                                            <p>{{ $ad->favorites->count() }}</p>
+                                        </span>
+                                    @else
+                                        <span wire:click="liker({{ $ad }})" class="d-flex ads_favorites_flex"><i
+                                                class="bi bi-heart"></i>
+                                            <p>{{ $ad->favorites->count() }}</p>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
 
+
+
+                        </div>
                     </div>
-                </div>
-            </div>
-
-            @endforeach
+                @endforeach
 
             </div>
         </div>
