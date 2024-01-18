ba81d61cb0d2866b6dc1c2a11aa7b21c92634e39
Fabrizio Angius <fabrizio.angius.4@gmail.com>
2024-01-18 21:07:55 +0100

    fix navbar

--- resources/views/components/navbar-main.blade.php
+++ resources/views/components/navbar-main.blade.php
@@ -1,116 +1,93 @@
 <nav class="  p-0">
     <div class="navbar_main">
-
+        
         <div class="navbar_component ">
-
             <ul class="">
                 <li class="navbar_home_button" >
-
-                    <a href="">
-                        Presto.it
-                    </a> 
-
+                    <span>Presto.it</span> 
                 </li>
             </ul>
-
         </div>
 
-
-
-        <div class="big_nav">
-            <ul class=" navbar_component">
-                <li class="nav_start 
-                 text-white @if(Illuminate\Support\Facades\Route::currentRouteName() == 'home') carent_root @endif ">
-
-                    <a href="{{ route('home') }}" class="">Home</a>
-                </li>
-
+    <div class="big_nav">
+        <ul class=" navbar_component">
+            <a href="{{ route('home') }}" class="">
+            <li class="nav_start @if(Illuminate\Support\Facades\Route::currentRouteName() == 'home') carent_root @endif ">
+            Home
+            </li></a>
 
                 {{-- REGISTER E LOGIN --}}
-                @guest
-                    <li class=" @if(Illuminate\Support\Facades\Route::currentRouteName() == 'register') carent_root @endif">
-                        <a href="{{ route('register') }}">
-                            Register</a>
+
+            @guest
+            <a href="{{ route('register') }}">
+            <li class=" @if(Illuminate\Support\Facades\Route::currentRouteName() == 'register') carent_root @endif">
+            Register</li></a>
+        
+            <a href="{{ route('login') }}">
+            <li class=" @if(Illuminate\Support\Facades\Route::currentRouteName() == 'login') carent_root @endif">
+            Login</li></a>
+
+            <a href="{{ route('ad.create') }}">
+            <li class="nav_end @if(Illuminate\Support\Facades\Route::currentRouteName() == 'ad.create') carent_root @endif">
+            Inserisci annuncio</li></a>
+                    
+            @endguest
+
+
+            {{-- OPZIONI ANNUNCI --}}
+
+            @auth
+            <a class="nav-link  " href="{{ route('ad.create') }}">
+            <li class=" @if(Illuminate\Support\Facades\Route::currentRouteName() == 'ad.create') carent_root @endif">
+            Inserisci annuncio</li></a>
+
+            <a class="nav-link" href="{{ route('ads.index') }}">       
+            <li class="@if(Illuminate\Support\Facades\Route::currentRouteName() == 'ads.index') carent_root @endif">
+            Tutti gli annunci</li></a>
+
+            @endauth
+
+
+            {{-- DROPDWON PROFILO --}}
+                
+            @auth
+            
+            <li class=" dropdown nav_end  @if(Illuminate\Support\Facades\Route::currentRouteName() == 'user.auth.profile') carent_root @endif">
+            <a class="nav-link dropdown-toggle " role="button" data-bs-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }} </a>
+                
+            <ul class="dropdown-menu ">
+                    <li>
+                        <a class="dropdown-item" href="{{route('user.auth.profile')}}">Il mio profilo</a>
                     </li>
-
-                    <li class=" @if(Illuminate\Support\Facades\Route::currentRouteName() == 'login') carent_root @endif">
-                      
-                        <a href="{{ route('login') }}">
-                            Login</a>
-                    </li>
-
-                    <li class="nav_end 
-                    @if(Illuminate\Support\Facades\Route::currentRouteName() == 'ad.create') carent_root @endif
-                    ">
-                        <a href="{{ route('ad.create') }}">Inserisci annuncio</a>
-                    </li>
-                @endguest
-
-
-                {{-- OPZIONI ANNUNCI --}}
-                @auth
-
-                    <li class="
-                    @if(Illuminate\Support\Facades\Route::currentRouteName() == 'ad.create') carent_root @endif
-                    ">
-                        <a class="nav-link  " href="{{ route('ad.create') }}">
-                            Inserisci annuncio</a>
-                    </li>
-
-                    <li class="
-                    @if(Illuminate\Support\Facades\Route::currentRouteName() == 'ads.index') carent_root @endif
-                    ">
-                        <a class="nav-link" href="{{ route('ads.index') }}">Tutti gli annunci</a>
-                    </li>
-
-                @endauth
-
-                {{-- drpdown --}}
-                @auth
-                    <li class=" dropdown nav_end  
-                    @if(Illuminate\Support\Facades\Route::currentRouteName() == 'user.auth.profile') carent_root @endif
-                    ">
-                        <a class="nav-link dropdown-toggle " role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">{{ Auth::user()->name }} </a>
-                        <ul class="dropdown-menu ">
-
-                            <li>
-                                <a class="dropdown-item" href="{{route('user.auth.profile')}}">Il mio profilo</a>
-                            </li>
                             
-                            @if(auth()->user()->is_revisor)
-                            <li>
-                                <a class="dropdown-item" href="{{route('revisor.index')}}">Zona revisore</a>
-                                <span class="position-absolute top-0 start-100 translate-niddle badge rounded-pill bg-danger" >
-                                    {{App\Models\Ad::toBeRevisionedCount()}}
-                                    <span>
-                                        unread messages
-                                    </span>
-                                </span>
+            @if(auth()->user()->is_revisor)
+                <li class="position-relative">
+                    <a class="dropdown-item" href="{{route('revisor.index')}}">Zona revisore</a>
+                    <span class="badge_revisor translate-niddle badge rounded-pill bg-danger" >
+                        {{App\Models\Ad::toBeRevisionedCount()}}<span> da revisionare</span>
+                </li>
+                            
+                @endif
+                <li>
+                    <a class="dropdown-item" href="">Opzione 3</a>
+                </li>
 
-                            </li>
-                            @endif
 
-                            <li>
-                                <a class="dropdown-item" href="">Opzione 3</a>
-                            </li>
+                {{-- LOGOUT --}}
 
-                            {{-- LOGOUT --}}
-                            <li><a class="dropdown-item " href="{{ route('logout') }}"
-                                    onclick="event.preventDefault(); getElementById('form-logout').submit();">Logout</a>
-                            </li>
-                            <form id="form-logout" action="/logout" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </ul>
-                    </li>
+                <li>
+                    <a class="dropdown-item " href="{{ route('logout') }}" onclick="event.preventDefault(); getElementById('form-logout').submit();">Logout</a>
+                </li>
+                <form id="form-logout" action="/logout" method="POST" class="d-none">
+                @csrf
+                </form>
 
-                @endauth
             </ul>
+            </li>
 
-        </div>
-
+            @endauth
+        </ul>
     </div>
 
-
+    </div>
 </nav>
