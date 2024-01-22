d2288870a5862f9418bf7bd73a2e45e65950f5e1
Ranveer Shekhawat <Ranveershekhawat58@gmail.com>
2024-01-22 21:06:18 +0100

    creato profilo seller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,5 +10,9 @@ class UserController extends Controller
     public function profile(){
         
         return view('user.auth.profile');
+    }
+    public function seller(User $user){
+        
+        return view('user.sellerProfile',compact('user'));
     }
 }
