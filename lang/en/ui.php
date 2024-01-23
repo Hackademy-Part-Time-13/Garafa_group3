b26232269ac16457958e92ebb741ad00d36b6a61
gabriele <cesaranogabriele02@gmail.com>
2024-01-23 18:36:28 +0100

    language

--- lang/en/ui.php
+++ lang/en/ui.php
@@ -1,6 +1,7 @@
 <?php
 return[
     'homept1'=>' Welcome to Shopping Paradise:',
-
-    'homept2'=>'Find Your Favorite Product at Affordable Prices!'
+    'homept2'=>'Find Your Favorite Product at Affordable Prices!',
+    'createad'=>'Insert ad',
+    
 ];
--- lang/it/ui.php
+++ lang/it/ui.php
@@ -1,6 +1,7 @@
 <?php
 return[
     'homept1'=>' Benvenuto nel Paradiso dello Shopping:',
-    'homept2'=>'Trova il Tuo Prodotto Preferito a Prezzi Convenienti!'
+    'homept2'=>'Trova il Tuo Prodotto Preferito a Prezzi Convenienti!',
+    'createad'=>'inserisci annuncio',
     
 ];
