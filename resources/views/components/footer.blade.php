28309e2139f906029520e0d02baaf73f615a3c88
Ranveer Shekhawat <Ranveershekhawat58@gmail.com>
2024-01-31 18:56:04 +0100

    footer responsiv

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -18,29 +18,34 @@
 
     <div class="container ">
         <div class="row">
-            <div class="col-xs-12 col-xl-4  cccccc">
+            <div class="col-xs-12 col-xl-4  ">
+                <div class="footer_text_cont">
+                    <h5>{{ __('ui.contact-us') }}</h5>
+                    <p>{{ env('MAIL_FROM_ADDRESS') }}</p>
+                    <p>tel. 7842548512</p>
+                    <p>via garafa</p>
+                    <p>00159 Torino&#40 TO &#41</p>
+                </div>
 
-                <h5>{{ __('ui.contact-us') }}</h5>
-                <p>{{ env('MAIL_FROM_ADDRESS') }}</p>
-                <p>tel. 7842548512</p>
-                <p>via garafa</p>
-                <p>00159 Torino&#40 TO &#41</p>
 
 
             </div>
-            <div class="col-xs-6 col-xl-4   cccccc">
-                <h5>{{ __('ui.explore') }}</h5>
-                <p>{{ __('ui.ads') }}</p>
-                <p>{{ __('ui.careers') }}</p>
-                <p>{{ __('ui.who') }}</p>
-                <p>{{ __('ui.privacy') }}</p>
-                <p>{{ __('ui.cookie') }}</p>
+            <div class="col-xs-6 col-xl-4   ">
+                <div class="footer_text_cont">
+                    <h5>{{ __('ui.explore') }}</h5>
+                    <p>{{ __('ui.ads') }}</p>
+                    <p>{{ __('ui.careers') }}</p>
+                    <p>{{ __('ui.who') }}</p>
+                    <p>{{ __('ui.privacy') }}</p>
+                    <p>{{ __('ui.cookie') }}</p>
+                </div>
+
             </div>
 
-            <div class="col-xs-6 col-xl-4 mt-4 cccccc">
-                <h5>{{ __('ui.partener') }}</h5>
-                <!-- Grid container -->
-                <div class="">
+            <div class="col-xs-6 col-xl-4 mt-4 ">
+                <div class="footer_text_cont">
+                    <h5>{{ __('ui.partener') }}</h5>
+                    <!-- Grid container -->
                     <!-- Section: Social media -->
                     <section class="footer_social_logo">
                         <!-- Google -->
